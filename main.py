d7c608ca4ca81cdb1bead7e78281413d4abe18b3
Mher <99.p3rc3n7@gmail.com>
2025-04-03 08:58:14 +0400

    create a basic login automation flow

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-driver.get("http://localhost:5173/")
+driver.get("http://localhost:5173/login")
+
+time.sleep(3)
+
+username_field = driver.find_element(By.ID, "first-name-field")
+password_field = driver.find_element(By.ID, "last-name-field")
+submit = driver.find_element(By.ID, "signUpButton")
+
+username_field.send_keys("mher@gmail.com")  
+password_field.send_keys("Kepler22bb!Kepler22bb!")  
+submit.click()
 
 time.sleep(30)
 
